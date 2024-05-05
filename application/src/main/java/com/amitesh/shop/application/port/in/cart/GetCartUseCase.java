be7fe5432c9d8f0b5a8b09f57834acc700e5cc5e
amitesh4u <amitesh4ever@gmail.com>
2024-05-05 21:30:24 +0530

    Implemented Port for Get Cart use case

--- application/src/main/java/com/amitesh/shop/application/port/in/cart/GetCartUseCase.java
+++ application/src/main/java/com/amitesh/shop/application/port/in/cart/GetCartUseCase.java
@@ -1,5 +1,9 @@
 package com.amitesh.shop.application.port.in.cart;
 
+import com.amitesh.shop.model.cart.Cart;
+import com.amitesh.shop.model.customer.CustomerId;
+
 public interface GetCartUseCase {
 
+  Cart getCart(CustomerId customerId);
 }
--- application/src/main/java/com/amitesh/shop/application/service/cart/GetCartService.java
+++ application/src/main/java/com/amitesh/shop/application/service/cart/GetCartService.java
@@ -1,7 +1,25 @@
 package com.amitesh.shop.application.service.cart;
 
 import com.amitesh.shop.application.port.in.cart.GetCartUseCase;
+import com.amitesh.shop.application.port.out.persistence.CartRepository;
+import com.amitesh.shop.model.cart.Cart;
+import com.amitesh.shop.model.customer.CustomerId;
 
 public class GetCartService implements GetCartUseCase {
 
+  private final CartRepository cartRepository;
+
+  public GetCartService(CartRepository cartRepository) {
+    this.cartRepository = cartRepository;
+  }
+
+  @Override
+  public Cart getCart(CustomerId customerId) {
+    if (null == customerId) {
+      throw new IllegalArgumentException("'customerId' must not be null");
+    }
+    return cartRepository
+        .findByCustomerId(customerId)
+        .orElseGet(() -> new Cart(customerId));
+  }
 }
