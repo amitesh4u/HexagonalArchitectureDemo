8959443bab40016194675bd4eac91bcf2bf6d434
amitesh4u <amitesh4ever@gmail.com>
2024-05-11 21:54:38 +0530

    Clean up

---
+++ bootstrap/src/test/java/com/amitesh/shop/bootstrap/HexagonalArchitectureShoppingApplicationTest.java
@@ -0,0 +1,14 @@
+package com.amitesh.shop.bootstrap;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchitectureShoppingApplicationTest {
+
+  @DisplayName("To verify if the application is able to load Spring context successfully or not.")
+  @Test
+  void contextLoads() {}
+}
+
